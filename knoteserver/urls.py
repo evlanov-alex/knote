5e12ecc1a2316f3cb324433a9b41c33dc6d14071
mirrorrim <hieronymus1100@gmail.com>
2019-01-12 14:27:36 +0300

    flake8 urls.py and init

--- knoteserver/urls.py
+++ knoteserver/urls.py
@@ -1,4 +1,4 @@
-"""knoteserver URL Configuration
+"""knoteserver URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -12,9 +12,10 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Knote API')
