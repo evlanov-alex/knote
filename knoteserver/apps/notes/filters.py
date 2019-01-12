c922de4bddb2a8bc2c23d85fd28de29a0e9f5abc
mirrorrim <hieronymus1100@gmail.com>
2019-01-12 15:22:57 +0300

    flake notes/filters

--- knoteserver/apps/notes/filters.py
+++ knoteserver/apps/notes/filters.py
@@ -4,21 +4,22 @@ from django_filters import rest_framework as filters
 
 from knoteserver.apps.notes.models import Note
 
-
 EMPTY_VALUES = (None, '', [], (), {})
 
 
 class NotesFilterSet(filters.FilterSet):
+    """FilterSet for notes view."""
     tags = filters.CharFilter(method='filter_by_tags')
     username = filters.CharFilter(method='filter_by_username')
     ordering = filters.OrderingFilter(
         fields=(
             ('created_at', 'created'),
             ('updated_at', 'updated'),
-        )
+        ),
     )
 
     def filter_queryset(self, queryset):
+        """Filter out empty usernames."""
         # handling username empty value here cause of filter_by_username wont be called when value is empty
         username_value = self.form.cleaned_data['username']
         if username_value in EMPTY_VALUES:
@@ -26,22 +27,24 @@ class NotesFilterSet(filters.FilterSet):
 
         return super(NotesFilterSet, self).filter_queryset(queryset)
 
-    class Meta:
+    class Meta:  # noqa: Z306,D106
         model = Note
         fields = ()
 
-    def filter_by_tags(self, queryset, name, value):
-        if value in EMPTY_VALUES:
+    def filter_by_tags(self, queryset, name, tags):
+        """Filter notes by tags."""
+        if tags in EMPTY_VALUES:
             return queryset
 
-        tags = value.split(',')
+        tags = tags.split(',')
         for tag in tags:
             queryset = queryset.filter(tags__name=tag.strip())
 
         return queryset
 
-    def filter_by_username(self, queryset, name, value):
-        user = get_object_or_404(User, username=value)
+    def filter_by_username(self, queryset, name, username):
+        """Filter notes b username."""
+        user = get_object_or_404(User, username=username)
 
         if user == self.request.user:
             return queryset.filter(author__user=self.request.user)
