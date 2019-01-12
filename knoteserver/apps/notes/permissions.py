8469f3f703cb4a16b64fd5e66281043f7546c0f7
mirrorrim <hieronymus1100@gmail.com>
2019-01-12 15:17:41 +0300

    flake notes/permissions

--- knoteserver/apps/notes/permissions.py
+++ knoteserver/apps/notes/permissions.py
@@ -2,22 +2,26 @@ from rest_framework.permissions import BasePermission
 
 
 class NotePermission(BasePermission):
-    READ_METHODS = {'GET', 'HEAD', 'OPTIONS'}
-    WRITE_METHODS = {'PUT', 'PATCH'} # without DELETE method, its allowed only for owner
+    """Permission class for controlling access for Notes."""
+
+    read_methods = {'GET', 'HEAD', 'OPTIONS'}
+    write_methods = {'PUT', 'PATCH'}  # without DELETE method, its allowed only for owner
 
     def has_permission(self, request, view):
+        """Has permission only if user authenticated."""
         return request.user and request.user.is_authenticated
 
-    def has_object_permission(self, request, view, obj):
-        if obj.author.user == request.user:
+    def has_object_permission(self, request, view, note):
+        """Check if current authenticated has any permissions for notes objects."""
+        if note.author.user == request.user:
             return True
 
-        if obj.allowed_profiles.filter(user=request.user).exists():
-            if request.method in self.READ_METHODS:
+        if note.allowed_profiles.filter(user=request.user).exists():
+            if request.method in self.read_methods:
                 return True
 
-            if request.method in self.WRITE_METHODS:
-                access = obj.access.get(profile__user=request.user)
+            if request.method in self.write_methods:
+                access = note.access.get(profile__user=request.user)
                 return access.can_write
 
         return False
