c2e676d6f3396b2eecbcdb48af47dba21fb33ced
Lexa <alexei03091997@gmail.com>
2019-01-21 22:05:08 +0300

    added /me endpoint

--- knoteserver/apps/profiles/urls.py
+++ knoteserver/apps/profiles/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from knoteserver.apps.profiles.views import ProfileViewSet
+from knoteserver.apps.profiles import views
 
 app_name = 'Profiles'
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'profiles', ProfileViewSet)
+router.register(r'profiles', views.ProfileViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('me', views.CurrentUserAPIView.as_view(), name='me'),
 ]
